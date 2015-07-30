f987c9796895cfc17f73341780009b2f10583072
Matt Bengston <matt.bengston@icfi.com>
2015-07-30 09:07:27 -0400

    Added handling for {{#unless var}}{{/unles}}

---
+++ build/jsp/testUnless/user-block.jsp
@@ -0,0 +1,3 @@
+<c:if test="${!userBlock.ignoreUser}">
+    <div>This will only show if we are not ignoring the user.</div>
+</c:if>
--- gulp-utils/statements-hbsToJsp.js
+++ gulp-utils/statements-hbsToJsp.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
-    name: "var",
-    regex: /(\{{2}([\w.\/]*)\}{2}|\{{2} ([\w.\/]*) \}{2})/,
+    name: "{{var}}",
+    regex: /(\{{2}(\w+[\w.\/]*)\}{2}|\{{2} ([\w.\/]*) \}{2})/,
     replace: function replaceVar(match) {
       var content = match[2] || match[3];
       console.log(match[0], '->', '${'+content+'}');
@@ -9,20 +9,37 @@ module.exports = [
     }
   },
   {
-    name: "if",
+    name: "{{#if test}}",
     regex: /(\{{2}\#if ([\w.\/\=\!\-\+\%\| \'\"\>\<]*)\}{2})/,
     replace: function replaceIf(match) {
-      var test = match[2];
-      console.log(match[0], '->', '<c:if test="${'+test+'}">');
-      return match['input'].replace(match[0], '<c:if test="${'+test+'}">');
+      var content = match[2];
+      console.log(match[0], '->', '<c:if test="${'+content+'}">');
+      return match['input'].replace(match[0], '<c:if test="${'+content+'}">');
     }
   },
   {
-    name: "/if",
+    name: "{{/if}}",
     regex: /(\{{2}\/if\}{2})/,
     replace: function replaceEndIf(match) {
       console.log('{{/if}}', '->', '</c:if>');
       return match['input'].replace(match[0], "</c:if>");
     }
-  }
+  },
+  {
+    name: "{{#unless test}}",
+    regex: /(\{{2}\#unless ([\w.\/\=\!\-\+\%\| \'\"\>\<]*)\}{2})/,
+    replace: function replaceElse(match) {
+      var content = match[2];
+      console.log(match[0], '->', '<c:if test="${!'+content+'}">');
+      return match['input'].replace(match[0], '<c:if test="${!'+content+'}">');
+    }
+  },
+  {
+    name: "{{/unless}}",
+    regex: /(\{{2}\/unless\}{2})/,
+    replace: function replaceEndIf(match) {
+      console.log('{{/unless}}', '->', '</c:if>');
+      return match['input'].replace(match[0], "</c:if>");
+    }
+  },
 ];
